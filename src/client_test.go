a198cbd5108eee48fea7c53ff301f93ebc46657f
werty144 <anton-paramonov2000@yandex.ru>
2023-06-26 17:20:54 +0200

    tests done

--- src/client_test.go
+++ src/client_test.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"math/rand"
 	"net"
+	"sort"
+	"sync"
 	"testing"
+	"time"
 )
 
-func TestPlayGame(t *testing.T) {
+func TestGetNetworkValue(t *testing.T) {
 	stopChanel := make(chan struct{})
 	defer close(stopChanel)
 
@@ -26,5 +30,51 @@ func TestPlayGame(t *testing.T) {
 	}
 	if v != trueValue {
 		t.Fail()
+	}
+}
+
+func TestGetMode(t *testing.T) {
+	values := []int{1, 1, 2, 2, 1, 3}
+	mode := getMode(values)
+	if mode != 1 {
+		t.Fail()
+	}
+}
+
+func TestGetModeAmbiguous(t *testing.T) {
+	values := []int{1, 1, 2, 2, 1, 2, 3}
+	mode := getMode(values)
+	if mode != 1 && mode != 2 {
+		t.Fail()
+	}
+}
+
+func TestThreadSafeSlice_Append(t *testing.T) {
+	rand.Seed(time.Now().UnixNano())
+	var values ThreadSafeSlice
+	var wg sync.WaitGroup
+	nWorkers := 1000
+	wg.Add(nWorkers)
+
+	for i := 0; i < nWorkers; i++ {
+		go func(v int) {
+			defer wg.Done()
+			delay := rand.Intn(10) + 1
+			time.Sleep(time.Duration(delay) * time.Millisecond)
+			values.Append(v)
+		}(i)
+	}
+	wg.Wait()
+
+	if len(values.slice) != nWorkers {
+		t.Fail()
+	}
+
+	sort.Ints(values.slice)
+
+	for i := 0; i < nWorkers; i++ {
+		if values.slice[i] != i {
+			t.Fail()
+		}
 	}
 }
